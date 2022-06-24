0f995ec7c61b98d7a9475e63eca3c284fe5651d8
shadowfake <iamshadowfake@gmail.com>
2022-06-24 22:59:25 +0300

    added: index, show and store in CalendarController.php; CalendarRequest.php; CalendarResource.php

---
+++ app/Http/Controllers/Api/CalendarController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CalendarRequest;
+use App\Http\Resources\CalendarResource;
+use App\Models\Calendar;
+use Illuminate\Http\Request;
+
+class CalendarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        return CalendarResource::collection(Calendar::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CalendarRequest $request)
+    {
+        $newEntry = Calendar::create($request->validated());
+
+        return new CalendarResource($newEntry);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new CalendarResource(Calendar::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Resources/CalendarResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CalendarResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'date' => $this->date,
+            'duration' => $this->duration,
+            'title' => $this->title,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
